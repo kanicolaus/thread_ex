158cfd8287c69e78e5f97ea7d288198e5b4e0cb1
Karl Nicolaus <karl@karlnicolaus.com>
2018-03-08 00:42:53 -0500

    Added c2.cc and e2.h

---
+++ e2.h
@@ -0,0 +1,35 @@
+
+//
+// Created by Brian Swenson on 2/8/18.
+//
+//#pragma once
+//#include <thread>
+
+#ifndef E2_E2_H
+#define E2_E2_H
+// FUNCTION PROTOTYPE
+/*
+int calcMin(const std::vector<int>& vec) {
+
+}
+int calcMax(const std::vector<int>& vec) {
+    int t = vec[0];
+    for(int i=0; i<vec.capacity(); ++i) {
+        t = (t < vec[i]) ? vec[i] : t;
+    }
+    return t;
+}
+long calcSum(const std::vector<int>& vec) {
+    long t = 0;
+    for (auto& n : vec)
+    t += n;
+    return t;
+}
+double calcAvg(const std::vector<int>& vec) {
+    float t = 0;
+    for (auto& n : vec)
+    t += n;
+    return t/vec.capacity();
+}
+*/
+#endif //E2_E2_H
